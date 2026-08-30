721fa8b01c344064fa9e1a6fadaa0c2eaeb9e36f
emmacjay9 <emmacjay9@gmail.com>
2026-08-30 07:50:51 +0100

    Add initial Sanity configuration file

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+
+export default defineConfig({
+  name: "default",
+  title: "Makascare Lifestyle",
+
+  projectId: "kmh1m4dn",
+  dataset: "production",
+
+  basePath: "/studio",
+
+  plugins: [structureTool()],
+
+  schema: {
+    types: [],
+  },
+});
